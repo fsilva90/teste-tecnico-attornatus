0cc564f0945ed5eab83819694e2863d61e4abad5
META\felipe.dasilva <felipe.dasilva@meta.com.br>
2023-09-28 10:45:13 -0300

    Tratar requisição invalida

--- src/main/java/com/testetecnicoattornatus/configuration/ExceptionResponseHandler.java
+++ src/main/java/com/testetecnicoattornatus/configuration/ExceptionResponseHandler.java
@@ -3,14 +3,28 @@ package com.testetecnicoattornatus.configuration;
 import com.testetecnicoattornatus.exception.EnderecoInexistenteException;
 import com.testetecnicoattornatus.exception.PessoaInexistenteException;
 import com.testetecnicoattornatus.dto.error.MensagemErro;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        return new ResponseEntity<>(new MensagemErro(ex.getMessage()),HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(PessoaInexistenteException.class)
     public ResponseEntity<MensagemErro> handlePessoaInexistente(PessoaInexistenteException ex) {
