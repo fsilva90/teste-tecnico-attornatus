cda30e2bc13a4a1229e85dd85d98d0b59cfb4e1c
Felipe Silva <47543121+fsilva90@users.noreply.github.com>
2023-09-30 23:22:57 -0300

    Update ConstanteUtil.java

--- src/main/java/com/testetecnicoattornatus/util/ConstanteUtil.java
+++ src/main/java/com/testetecnicoattornatus/util/ConstanteUtil.java
@@ -2,11 +2,11 @@ package com.testetecnicoattornatus.util;
 
 public final class ConstanteUtil {
 
-    private ConstanteUtil() {
-    }
-
     public static final String MENSAGEM_PESSOA_INEXISTENTE = "Pessoa id %s não existe.";
     public static final String MENSAGEM_ENDERECO_INEXISTENTE = "Nenhum endereço encontrado para a pessoa %s .";
     public static final String MENSAGEM_ENDERECO_PRINCIPAL_INEXISTENTE = "Não existe endereço principal para pessoa id %s.";
 
+    private ConstanteUtil() {
+    }
+    
 }
